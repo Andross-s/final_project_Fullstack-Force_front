ba6dabc489f2c17b869d1e1cff6853d5ce7fbe8c
Dina Muravska <dina.muravska.25@gmail.com>
2026-06-16 20:15:45 +0300

    fix

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: NextRequest) {
   console.log('LOGIN ROUTE');
   try {
     const body = await req.json();
-    const apiRes = await api.post('auth/login', body);
+    const apiRes = await api.post('/auth/login', body);
 
     const cookieStore = await cookies();
     const setCookie = apiRes.headers['set-cookie'];
