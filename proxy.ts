8057ca7ad5f1899732e086b25b3742331ecf298f
Andrij Skoropad <andross_s@ukr.net>
2026-06-18 11:50:57 +0300

    Fix proxy

--- proxy.ts
+++ proxy.ts
@@ -26,43 +26,43 @@ export async function proxy(request: NextRequest) {
   const isPrivateRoute = privateRoutes.some(route => pathname.startsWith(route));
   // Якщо це приватний маршрут :
   if (!accessToken) {
-    if (refreshToken) {
-      // Якщо accessToken відсутній, але є refreshToken — потрібно перевірити сесію навіть для публічного маршруту,
-      // адже сесія може залишатися активною, і тоді потрібно заборонити доступ до публічного маршруту.
-      const data = await checkServerSession();
-      const setCookie = data.headers['set-cookie'];
+    // if (refreshToken) {
+    //   // Якщо accessToken відсутній, але є refreshToken — потрібно перевірити сесію навіть для публічного маршруту,
+    //   // адже сесія може залишатися активною, і тоді потрібно заборонити доступ до публічного маршруту.
+    //   const data = await checkServerSession();
+    //   const setCookie = data.headers['set-cookie'];
 
-      if (setCookie) {
-        const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
-        for (const cookieStr of cookieArray) {
-          const parsed = parse(cookieStr);
-          const options = {
-            expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
-            path: parsed.Path,
-            maxAge: Number(parsed['Max-Age']),
-          };
-          if (parsed.accessToken) cookieStore.set('accessToken', parsed.accessToken, options);
-          if (parsed.refreshToken) cookieStore.set('refreshToken', parsed.refreshToken, options);
-        }
-        // Якщо сесія все ще активна:
-        // для публічного маршруту — виконуємо редірект на головну.
-        if (isPublicRoute) {
-          return NextResponse.redirect(new URL('/', request.url), {
-            headers: {
-              Cookie: cookieStore.toString(),
-            },
-          });
-        }
-        // для приватного маршруту — дозволяємо доступ
-        if (isPrivateRoute) {
-          return NextResponse.next({
-            headers: {
-              Cookie: cookieStore.toString(),
-            },
-          });
-        }
-      }
-    }
+    //   if (setCookie) {
+    //     const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
+    //     for (const cookieStr of cookieArray) {
+    //       const parsed = parse(cookieStr);
+    //       const options = {
+    //         expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
+    //         path: parsed.Path,
+    //         maxAge: Number(parsed['Max-Age']),
+    //       };
+    //       if (parsed.accessToken) cookieStore.set('accessToken', parsed.accessToken, options);
+    //       if (parsed.refreshToken) cookieStore.set('refreshToken', parsed.refreshToken, options);
+    //     }
+    //     // Якщо сесія все ще активна:
+    //     // для публічного маршруту — виконуємо редірект на головну.
+    //     if (isPublicRoute) {
+    //       return NextResponse.redirect(new URL('/', request.url), {
+    //         headers: {
+    //           Cookie: cookieStore.toString(),
+    //         },
+    //       });
+    //     }
+    //     // для приватного маршруту — дозволяємо доступ
+    //     if (isPrivateRoute) {
+    //       return NextResponse.next({
+    //         headers: {
+    //           Cookie: cookieStore.toString(),
+    //         },
+    //       });
+    //     }
+    //   }
+    // }
     // Якщо refreshToken або сесії немає:
     // публічний маршрут — дозволяємо доступ
     if (isPublicRoute) {
