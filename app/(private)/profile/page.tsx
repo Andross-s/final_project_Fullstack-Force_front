e231aad4198b8d66994fac1e262fd81fa1b72397
Dina Muravska <dina.muravska.25@gmail.com>
2026-06-19 21:02:39 +0300

    add fix

--- app/(private)/profile/page.tsx
+++ app/(private)/profile/page.tsx
@@ -1,17 +1,17 @@
-import ProfileNavigation from "@/components/profile/ProfileNavigation/ProfileNavigation";
-import Filters from "@/components/recipes/Filters/Filters";
-import RecipesList from "@/components/recipes/RecipesList/RecipesList";
-import LoadMoreBtn from "@/components/recipes/LoadMoreBtn/LoadMoreBtn";
-import styles from "./page.module.css";
+import ProfileNavigation from '@/components/profile/ProfileNavigation/ProfileNavigation';
+import Filters from '@/components/recipes/Filters/Filters';
+import RecipesList from '@/components/recipes/RecipesList/RecipesList';
+import LoadMoreBtn from '@/components/recipes/LoadMoreBtn/LoadMoreBtn';
+import styles from './page.module.css';
 
 const recipes = [
   {
-    id: "1",
-    title: "Test recipe",
-    description: "Recipe description",
-    time: "30 min",
+    id: '1',
+    title: 'Test recipe',
+    description: 'Recipe description',
+    time: '30 min',
     calories: 250,
-    image: "/default-image-desktop.jpg",
+    image: '/default-image-desktop.jpg',
   },
 ];
 
