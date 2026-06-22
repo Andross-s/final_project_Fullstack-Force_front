ff4e3b1bd600e7ff678dedc25aaf78a21d5f0e36
ImMima <228374461+ImMima@users.noreply.github.com>
2026-06-23 01:14:54 +0300

    not found and recipe details

--- components/recipes/recipeDetails/GeneralInfo/GeneralInfo.tsx
+++ components/recipes/recipeDetails/GeneralInfo/GeneralInfo.tsx
@@ -1,30 +1,33 @@
-"use client";
+'use client';
 
-import styles from "./GeneralInfo.module.css";
+import styles from './GeneralInfo.module.css';
 
 type GeneralInfoProps = {
-  time: number | string | null;
+  time?: number | string | null;
   calories?: number | null;
+  category?: string | null;
 };
 
-export default function GeneralInfo({ time, calories }: GeneralInfoProps) {
-  const formattedTime =
-    time !== null && time !== undefined && time !== ""
-      ? `${time} min`
-      : "—";
+export default function GeneralInfo({ time, calories, category }: GeneralInfoProps) {
+  const formattedTime = time !== null && time !== undefined && time !== '' ? `${time} min` : '—';
 
-  const formattedCalories =
-    calories !== null && calories !== undefined
-      ? `${calories} kcal`
-      : null;
-
+  const formattedCalories = calories !== null && calories !== undefined ? `${calories} kcal` : '—';
   return (
     <div className={styles.meta}>
-      <span>⏱ {formattedTime}</span>
+      <h3 className={styles.title}>General informations</h3>
+      <span className={styles.stat}>
+        <b className={styles.boldstat}>Category: </b>
+        {category ?? '-'}
+      </span>
+      <span className={styles.stat}>
+        <b className={styles.boldstat}>Cooking time: </b>
+        {formattedTime}
+      </span>
 
-      {formattedCalories && (
-        <span>🔥 {formattedCalories}</span>
-      )}
+      <span className={styles.stat}>
+        <b className={styles.boldstat}>Caloric content: </b>
+        {formattedCalories}
+      </span>
     </div>
   );
 }
--- components/recipes/recipeDetails/RecipeDetailsSkeleton.tsx
+++
@@ -1,3 +0,0 @@
-export default function RecipeDetailsSkeleton() {
-  return null;
-}
