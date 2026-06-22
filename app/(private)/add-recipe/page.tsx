cd672406260a6f2eeed2c46b74e272cf756f6aee
shumskiy <shumskiparents@gmail.com>
2026-06-22 09:30:04 +0200

    add route

--- app/(private)/add-recipe/page.tsx
+++ app/(private)/add-recipe/page.tsx
@@ -3,9 +3,10 @@ import AddRecipeForm from "@/components/recipes/AddRecipeForm/AddRecipeForm";
 export default function AddRecipePage() {
   return (
 <section>
-<div> <h1>Add recipe</h1>
-<p> Share your favorite recipe with the
-community and inspire others to cook something delicious.  </p>
+      <div>
+        
+        <h1>Add recipe</h1>
+        
         <AddRecipeForm />
       </div>
     </section>
